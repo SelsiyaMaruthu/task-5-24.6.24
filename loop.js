1ed5bf6ef39585efc5bd1dd34d5fbb9a7a112a71
SelsiyaMaruthu <selsiyamarudhu14@gmail.com>
2024-06-24 20:49:12 +0530

    first commit

---
+++ loop.js
@@ -0,0 +1,24 @@
+
+
+// for loop
+//The for statement defines a code block that is executed as long as a condition is true.
+//example:program to display text 5 times
+var mydetails1 = [{
+    "Name": "SELSIYA.M",
+    "Age":24,
+    "Degree":"B.E,computer science",
+    "location": {
+        "address": "4/91,temple street,keelamovarkarai",
+        "postalCode": 609106,
+        "city": "mayiladuthurai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    
+    "Number":9787548503,
+    
+    }]
+    for(var i=0;i<mydetails1.length;i++){
+        console.log(mydetails1[i]);
+        
+    }
---
+++ resume.js
@@ -0,0 +1,50 @@
+let myResume={
+    "basics": {
+      "name": "SELSIYA.M",
+      "email": "selsiyamarudhu14@gamil.com",
+      "phone": 9787548503,
+      "degree": "B.E,computer science",
+      "location": {
+        "address": "4/91,temple street,keelamovarkarai",
+        "postalCode": 609106,
+        "city": "mayiladuthurai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+           "github":"https://github.com/SelsiyaMaruthu"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "A.V.C College Of Engineering",
+        "department": "computer science",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "gpa": 8.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript,HTML,CSS",
+        "level": "beginer",
+        "project": [
+          "SAFETY HELMET DETECTION SYSTEM USING DEEP LEARNING TECHNIQUE"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "travelling",
+      }
+    ]
+  }
+  console.log(myResume);
